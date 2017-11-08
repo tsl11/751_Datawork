43e99fd619d103da6b6cff7e59dd7efec589cb53
Tammy Lee <tammylee@MacBook-Air-4.local>
2017-11-07 23:13:03 -0500

    facebook data cleaning by 11/7/2017

--- facebook/facebook_datacleaning.R
+++ facebook/facebook_datacleaning.R
@@ -8,10 +8,12 @@ library(KoNLP)
 library(lubridate)
 library(stringr)
 library(base64enc)
+library(reshape2)
 
 useSejongDic()
 buildDictionary(ext_dic = c('sejong', 'woorimalsam'),user_dic = data.frame("term"=c("이대", "최순실", "박근혜", "태블릿", "정유라"), "tag"=c("nqq", "nqpb", "nqpb", "f", "nqpb")))
 buildDictionary(ext_dic = c('sejong', 'woorimalsam'),user_dic = data.frame("term"=c("미르재단", "K스포츠재단"), "tag"=c("nqq", "nqq")))
+buildDictionary(ext_dic = c('sejong', 'woorimalsam'),user_dic = data.frame("term"=c("정윤회", "문건", "유출", "문건유출", "조응천", "최경위", "박관천", "비선", "문고리","십상시"), "tag"=c("nqpb","nqpb","nqpb","nqpb","nqpb","nqpb","nqpb","nqpb","nqpb","nqpb")))
 
 
 ## Cleaning each data frame
@@ -56,11 +58,32 @@ fbmedia_2016 = fbmedia_2016[index_2016, ]
 fbsummary_2016 = fbmedia_2016 %>% 
   group_by(created_time) %>% 
   summarise(nrows = n(), shares = sum(shares_count), likes = sum(likes_count), 
-            comments = sum(comments_count), totals = sum(activity_count))
+            comments = sum(comments_count), totals = sum(activity_count)) %>%
+  mutate(totals = likes + shares + comments)
 
 fbkeyword_2016 = fbmedia_2016$message %>% sapply(function(x) na.omit(x) %>% extractNoun())
 names(fbkeyword_2016) <- c(1:4846)  
 fbkeyword_2016 = unlist(fbkeyword_2016) %>% as.data.frame() %>% map_df(str_replace_all("-",""))
+
+##facebook 2014 data
+
+
+fbmedia_2014 = fbmedia %>%
+  mutate(created_time = as.Date(created_time)) %>%
+  filter(created_time > as.Date("2014-11-27") & created_time < as.Date("2015-03-06"))
+
+scandal_2014 = c("정윤회", "문건", "유출", "문건유출", "조응천", "최경위", "박관천", "비선", "문고리","십상시")
+index_2014 = fbmedia_2014$message %>% lapply(function(x) str_detect(x, scandal_2014) %>% any()) %>% unlist()
+
+fbmedia_2014 = fbmedia_2014[index_2014, ] 
+
+fbsummary_2014 = fbmedia_2014 %>% 
+  group_by(created_time) %>% 
+  summarise(nrows = n(), shares = sum(shares_count), likes = sum(likes_count), 
+            comments = sum(comments_count), totals = sum(activity_count)) %>%
+  mutate(totals = likes + shares + comments)
+
+
 
 # visualize evolution in metric
 library(ggplot2)
@@ -68,11 +91,20 @@ library(scales)
 library(reshape2)
 fbvis_2016 = fbsummary_2016 %>% select(c(created_time, shares, likes, comments, totals)) %>%
   melt(id.var="created_time") %>% group_by(variable) %>%
-  mutate(
-    cum_value = cumsum(value)
-  ) 
+  mutate(cum_value = cumsum(value)) 
 
 fbvis_2016 %>% ggplot(aes(x=created_time, y=cum_value, color=variable)) + geom_line(aes(alpha=0.8))
+
+fbsummary_2016 %>% ggplot(aes(x=created_time, y=totals)) + geom_line() + scale_x_date(date_breaks = "1 day", date_labels = "%b %d", limits = c(as.Date("2016-09-20"), as.Date("2016-10-07")))
+
+
+fbsummary_2016 %>% ggplot(aes(x=created_time, y=totals)) + geom_line() + scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
+
+fbvis_2014 = fbsummary_2014 %>% select(c(created_time, shares, likes, comments, totals)) %>%
+  melt(id.var="created_time") %>% group_by(variable) %>%
+  mutate(cum_value = cumsum(value)) 
+
+fbsummary_2014 %>% ggplot(aes(x=created_time, y=totals)) + geom_line() + scale_x_date(date_breaks = "1 week", date_labels = "%b %d")
 
 
 #########11/06/2017########################################################################  
