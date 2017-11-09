2fefdf74c44fe439552e1c07739bcc9becfcd9c9
Tammy Lee <tsl11@duke.edu>
2017-11-09 00:39:34 -0500

    upto 8273

--- naverNews/naverNewsbyKeyword.R
+++ naverNews/naverNewsbyKeyword.R
@@ -19,15 +19,15 @@ newsList = list()
 
 for (i in 1:length(qlist)){
   
-  print(paste0(qlist[i], "/ start Time: ", strTime," / spent Time: ", Sys.time()-midTime," / spent Time at first: ", Sys.time()-strTime))
+  print(paste0("/ start Time: ", strTime," / spent Time: ", Sys.time()-midTime," / spent Time at first: ", Sys.time()-strTime))
   midTime<-Sys.time()
-  pageUrli = paste0("https://search.naver.com/search.naver?where=news&query=",qlist[i],"&ie=utf8&sm=tab_srt&sort=1&photo=0&field=0&reporter_article=&pd=3&ds=",strDateo,"&de=",endDateo,"&docid=&nso=so%3Ar%2Cp%3Afrom",strDated,"to",endDated,"%2Ca%3Aall&mynews=0&mson=0&refresh_start=0&related=0")  
+  pageUrli = paste0("https://search.naver.com/search.naver?where=news&query=최순실&ie=utf8&sm=tab_srt&sort=1&photo=0&field=0&reporter_article=&pd=3&ds=",strDateo,"&de=",endDateo,"&docid=&nso=so%3Ar%2Cp%3Afrom",strDated,"to",endDated,"%2Ca%3Aall&mynews=0&mson=0&refresh_start=0&related=0")  
   
   maxArticle = read_html(pageUrli) %>% 
     html_node("#main_pack > div.news.mynews.section > div > div.title_desc.all_my > span") %>%
     html_text() %>% str_extract(.,"\\d{3,}\\,\\d{3,}") %>% str_replace(",","") %>% as.integer()
   
-  for (i in 1:{maxArticle %/% 10}){
+  for (i in 3472:{maxArticle %/% 10}){
     start = (i-1)*10 + 1
     print(paste0(i," / ",{maxArticle %/% 10}, "/ start Time: ", strTime," / spent Time: ", Sys.time()-midTime," / spent Time at first: ", Sys.time()-strTime))
     midTime<-Sys.time()
@@ -38,7 +38,7 @@ for (i in 1:length(qlist)){
 }
 
 
-## qlist[1] 3471까지 돌렸음 (11/8/2017)
+## qlist[1] 8273까지 돌렸음 (11/9/2017)
 
 page_content = map_df(newsList, function(x) x) 
 page_content = lapply(page_content$news_links, function(x) getContent(x))
